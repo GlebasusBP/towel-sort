0f4ead94b6efe80dc5033c72f9e7264c7fe21dc7
GlebasusBP <bgp13art@Gmail.com>
2023-01-15 11:39:24 +0300

    8 passing

--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let res = [];
+
+  if(matrix){
+    matrix.forEach( (arr, i) => {
+      if((i+1)%2 === 0){
+        arr.reverse().forEach( e => {
+        res.push(e);
+        })
+      } else {
+        arr.forEach( e => {
+        res.push(e);
+        }) 
+      }
+    })
+  } else {
+    return [];
+  }
+ 
+  return res;
 }
